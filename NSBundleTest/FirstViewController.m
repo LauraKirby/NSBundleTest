2acaf19e7f81e7668b024d9349f31b493dfd2d13
LauraKirby <laurakirby26@gmail.com>
2014-12-22 00:06:26 -0800

    NSBundleTest

---
+++ NSBundleTest/FirstViewController.m
@@ -0,0 +1,42 @@
+//
+//  FirstViewController.m
+//  NSBundleTest
+//
+//  Created by Glenna Buford on 12/19/14.
+//  Copyright (c) 2014 Glenna Buford. All rights reserved.
+//
+
+#import "FirstViewController.h"
+#import "SecondViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonPressed:(id)sender {
+    SecondViewController *secondVC = [[SecondViewController alloc] init];
+    [self showViewController:secondVC sender:self];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
